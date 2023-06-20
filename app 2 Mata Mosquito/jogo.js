c027210d08291ab5a01f7dd719796ce2d0ccdbce
kerrymuniz <kerrym877@gmail.com>
2023-06-20 14:58:41 -0300

    fim projeto mata Mosquito

--- app 2 Mata Mosquito/jogo.js
+++ app 2 Mata Mosquito/jogo.js
@@ -1,24 +1,64 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search     //o 'search' retorna tudo oque tiver depois da interrogação, inclusive ela mesma.
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+    //1500 ms
+    criaMosquitoTempo = 1500
+} else if(nivel === 'diicil') {
+    //1000 ms
+    criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+    //750 ms
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo() {
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 
     console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+
+},1000)
+
 function posicaoRandomica() {
 
     //remover mosquito anterior (caso exista)
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        } else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+            vidas++
+        }
     }
 
-    var posicaoX = Math.floor(Math.random() * largura) - 90
-    var posicaoY = Math.floor(Math.random() * altura) - 90
+    var posicaoX = Math.floor(Math.random() * largura - 90)
+    var posicaoY = Math.floor(Math.random() * altura - 90)
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -33,6 +73,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
